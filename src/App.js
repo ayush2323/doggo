be51513268c9a93653ed00a7c376c6f62870dc53
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2021-03-26 13:00:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-
+import react, {useState, useEffect} from 'react'
+import '../src/CSS/index.css'
 export default function App() {
+
+  const [breeds, setBreeds] = useState([]);
+  const [currentBreed, setCurrentBreed] = useState('affenpinscher')
+  const [showImage, setShowImage] = useState('');
+
+  const callApi = async () => {
+    try {
+        const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+        const dogBreed = await response.json();
+        setBreeds(dogBreed.message);
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  const getImage = async () => {
+    try {
+        const res = await fetch(`https://dog.ceo/api/breed/${currentBreed}/images/random`);
+        const image = await res.json();
+        console.log(image);
+        setShowImage(image);
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    callApi()
+  }, [])
+
+  useEffect(()=> {
+    getImage()
+  }, [currentBreed])
+
+  const changeHandler = (e) => {
+    console.log(currentBreed)
+    setCurrentBreed(e.target.value)
+  }
 
   return (
     <div className="App">
-	  <h1>hello world</h1>
+      <div className="container">
+        <h1> Paw Select</h1>
+        <div className="dropdown-div">
+          <select onChange={changeHandler}>
+              {Object.keys(breeds).map(breed => (
+                <option>{breed}</option>
+              ))}
+          </select>
+          
+        </div>
+        <div className="img-div">
+          <img src={showImage.message}></img>
+        </div>
+      </div>
     </div>
   );
 }
